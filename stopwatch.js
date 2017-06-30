8c7136b4261dde497a640d40e561da4444414eba
Guillermo <devivero@gmail.com>
2017-06-29 20:39:34 -0700

    added the rest of the functionality

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,21 @@
+// button controls
+const start = document.querySelector('button.start')
+const stop = document.querySelector('button.stop')
+const lap = document.querySelector('button.lap')
+const reset = document.querySelector('button.reset')
+
+// DOM elements that I need to update
+const lapList = document.querySelector('#lapList')
+const stopwatchTime = document.querySelector('#stopwatchTime')
+
+// constants that shouldn't ever change
+let laps = []
+const intervalRate = 10 // update the stopwatch every 10 milliseconds
+
+// values that will change pretty often
+let intervalId = null
+let rawTime = 0
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -8,6 +26,62 @@ function formatTime (raw) {
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
+// start the stopwatch by creating a new interval
+// we'll store the interval id so we can manipulate the interval later
+function stopwatchStart (event) {
+  event.preventDefault()
+  console.log('started!')
+
+  // every 10 milliseconds, update the stopwatch
+  intervalId = setInterval(stopwatchUpdate, intervalRate)
+}
+
+// adds the interval to the stopwatch time since the last "tick"
+// then update the DOM with the new stopwatch time
+function stopwatchUpdate () {
+  rawTime += intervalRate
+  stopwatchTime.innerHTML = formatTime(rawTime)
+}
+
+// stops the stopwatch by clearing the interval
+function stopwatchStop (event) {
+  event.preventDefault()
+  console.log('stopped!')
+
+  clearInterval(intervalId)
+}
+
+function stopwatchReset (event) {
+  event.preventDefault()
+  console.log('Reset!')
+
+  clearInterval(intervalId)
+
+  rawTime = 0;
+  stopwatchTime.innerHTML = formatTime(rawTime)
+  lapList.innerHTML = ""
+  laps = [];
+}
+
+function stopwatchLap (event) {
+  event.preventDefault()
+  console.log('Lap')
+
+  clearInterval(intervalId)
+
+  laps.push(rawTime)
+  lapList.innerHTML = ""
+
+  stopwatchTime.innerHTML = formatTime(rawTime)
+  lapList.innerHTML = ""
+
+  for (let i = 0; i < laps.length; i++) {
+            let lapTime = document.createElement("li");
+            lapTime.innerHTML = formatTime(laps[i]);
+            lapList.appendChild(lapTime);
+        }
+}
+
 // adds a leading zero because humans like them
 function zeroPad (value) {
   let pad = value < 10 ? '0' : ''
@@ -16,4 +90,10 @@ function zeroPad (value) {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+
+
+  start.addEventListener("click", stopwatchStart)
+  stop.addEventListener("click", stopwatchStop)
+  lap.addEventListener("click", stopwatchLap)
+  reset.addEventListener("click", stopwatchReset)
 })
